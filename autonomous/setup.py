4b8e5e96596e26da987c6505e32ff176ffb29a4a
wiktorj <wiktor.jurasz@gmail.com>
2025-08-07 09:09:55 +0200

    chore: update setup.py for Python 3.10+

--- autonomous/setup.py
+++ autonomous/setup.py
@@ -3,21 +3,14 @@ from setuptools import setup, find_packages
 setup(
     name="autonomous",
     version="0.1.0",
-    description="Autonomous robotics environments for reinforcement learning",
+    description="Autonomous robotics",
     packages=find_packages(),
-    install_requires=[
-        "gymnasium",
-        "mujoco",
-        "numpy",
-    ],
-    python_requires=">=3.8",
+    python_requires=">=3.10",
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
     ],

fix: correct end-effector rotation to prevent arm twisting

aera/autonomous/envs/ar4_mk3_base.py
@@ -89,7 +89,7 @@ class BaseEnv(MujocoRobotEnv):
         rot_ctrl = [
             1.0,
             0.0,
-            1.0,
+            0.0,
             0.0,
         ]  # fixed rotation of the end effector, expressed as a quaternion
         gripper_ctrl = np.array([gripper_ctrl, gripper_ctrl])